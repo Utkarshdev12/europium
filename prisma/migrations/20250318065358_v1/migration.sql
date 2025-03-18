a147550d05f3a4a123fa93e19ee4b2daf0023823
Utkarsh <utkarshdev12@gmail.com>
2025-03-18 15:06:28 +0530

    initialize repository

---
+++ prisma/migrations/20250318065358_v1/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import prisma from './prisma.js'
+
+const app = new Hono()
+
+
+app.get('/contacts', async (c) => {
+  const contacts = await prisma.contact.findMany()
+  return c.json(contacts)
+})
+
+app.post('/contacts', async (c) => {
+  const body = await c.req.json()
+  const { name, phone } = body
+
+  try {
+    const contact = await prisma.contact.create({
+      data: { name, phone },
+    })
+    return c.json(contact)
+  } catch (error) {
+    return c.json({ error: 'Unable to create contact' }, 400)
+  }
+})
+
+app.patch('/contacts/:id', async (c) => {
+  const id = c.req.param('id')
+  const body = await c.req.json()
+  const { name, phone } = body
+
+  try {
+    const updatedContact = await prisma.contact.update({
+      where: { id },
+      data: { name, phone },
+    })
+    return c.json(updatedContact)
+  } catch (error) {
+    return c.json({ error: 'Contact not found' }, 404)
+  }
+})
+
+app.delete('/contacts/:id', async (c) => {
+  const id = c.req.param('id')
+
+  try {
+    await prisma.contact.delete({
+      where: { id },
+    })
+    return c.json({ message: 'Contact deleted successfully' })
+  } catch (error) {
+    return c.json({ error: 'Contact not found' }, 404)
+  }
+})
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  }
+)
